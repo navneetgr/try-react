92d7da6a2064e1f32a7f4946531c4d7ad50e5a03
Navneet Gangwar <navneet.gr@yahoo.com>
2023-10-23 22:29:36 +0530

    use variable for create id

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+const greeting = "greeting";
+
 function App() {
   return (
     <div className="container">
-      <h1 id="greeting">Hello, World</h1>
+      <h1 id={greeting}>Hello, World</h1>
       <p>I am writing JSX</p>
     </div>
   );
