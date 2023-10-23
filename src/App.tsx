7868e713d9a4e8e41bcdb855971d0546ff439e2c
Navneet Gangwar <navneet.gr@yahoo.com>
2023-10-23 23:03:36 +0530

    mapping over data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React from "react";
 import "./App.css";
 
 const greeting = "greeting";
+const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face"
+  },
+  {
+    emoji: "🎉",
+    name: "party popper"
+  },
+  {
+    emoji: "💃",
+    name: "woman dancing"
+  }
+];
+
 const displayElementMsg = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => alert((event.target as HTMLElement).id)
 
 function App() {
@@ -10,27 +25,17 @@ function App() {
       <h1 id={greeting}>Hello, World</h1>
       <p>I am writing JSX</p>
       <ul>
-        <li>
-          <button onClick={displayElementMsg}>
-            <span role="img" aria-label="grinning face" id="grinning face">
-              😀
-            </span>
-          </button>
-        </li>
-        <li>
-          <button onClick={displayElementMsg}>
-            <span role="img" aria-label="party popper" id="party popper">
-              🎉
-            </span>
-          </button>
-        </li>
-        <li>
-          <button onClick={displayElementMsg}>
-            <span role="img" aria-label="woman dancing" id="woman dancing">
-              💃
-            </span>
-          </button>
-        </li>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayElementMsg}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
       </ul>
     </div>
   );
