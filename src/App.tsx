c9cc1ae0587badb57d9847e60feef414e52f227d
Navneet Gangwar <navneet.gr@yahoo.com>
2023-10-23 22:26:30 +0530

    add id

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello, World</h1>
+      <h1 id="greeting">Hello, World</h1>
       <p>I am writing JSX</p>
     </div>
   );
