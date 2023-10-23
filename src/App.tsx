14ac801059a846c8a61581087fe62d85196aa641
Navneet Gangwar <navneet.gr@yahoo.com>
2023-10-23 22:33:11 +0530

    add emoji and styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,23 @@ function App() {
     <div className="container">
       <h1 id={greeting}>Hello, World</h1>
       <p>I am writing JSX</p>
+      <ul>
+        <li>
+            <button>
+              <span role="img" aria-label="grinning face" id="grinning face">😀</span>
+            </button>
+        </li>
+        <li>
+          <button>
+              <span role="img" aria-label="party popper" id="party popper">🎉</span>
+          </button>
+        </li>
+        <li>
+          <button>
+              <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
